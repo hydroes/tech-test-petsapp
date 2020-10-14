7d04cb4f614f39ea365c818cb1253418af4e66e8
Brian Gouws <briangouws@hotmail.com>
2020-10-14 12:13:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// fn parse(datestring: DateString): Date;
+// fn stringify(date: Date): DateString;
+
+// @todo set timezone to 
+
+// Date.UTC(year[, month[, day[, hour[, minute[, second[, millisecond]]]]]])
+const utcDate1 = new Date(Date.UTC(96, 1, 2, 3, 4, 5));
+const utcDate2 = new Date(Date.UTC(0, 0, 0, 0, 0, 0));
+
+console.log(utcDate1.toUTCString());
+// expected output: Fri, 02 Feb 1996 03:04:05 GMT
+
+console.log(utcDate2.toUTCString());
+// expected output: Sun, 31 Dec 1899 00:00:00 GMT
